22fccbaf4949f9e83ff053057024618f99f4836b
Raul Dipeas <rauldipeas@disroot.org>
2022-06-02 14:12:51 -0300

    ^update

--- postinst/optional/gemfury.sh
+++ postinst/optional/gemfury.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
+# Fury CLI
 echo\
     'deb [trusted=yes] https://cli.gemfury.com/apt/ /'|\
     sudo tee\
     /etc/apt/sources.list.d/fury-cli.list
 sudo apt update
 sudo nala install -y fury-cli
+
+# Raul Dipeas APT
+echo\
+    'deb [trusted=yes] https://rauldipeas.fury.site/apt/ /'|\
+    sudo tee\
+    /etc/apt/sources.list.d/rauldipeas.list
+cat <<EOF |sudo tee /etc/apt/auth.conf.d/rauldipeas.conf
+machine rauldipeas.fury.site
+    login 1FGwwq-ATdOQ6SkGhvbxI6jxQAp5pEZxw
+    password NOPASS
+EOF
