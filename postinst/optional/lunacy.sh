28c741582e3940cf80eb7d609dae2fce319ea72c
Raul Dipeas <rauldipeas@disroot.org>
2022-06-11 17:49:15 -0300

    ^postinst

--- postinst/optional/lunacy.sh
+++ postinst/optional/lunacy.sh
@@ -5,5 +5,6 @@ aria2c --console-log-level=error --summary-interval=0\
 	https://lun-eu-static.s3.eu-central-003.backblazeb2.com/setup/Lunacy.deb
 sudo apt install ./Lunacy.deb
 sudo sed -i\
-	's@/opt/icons8/lunacy/Assets/LunacyLogo.png@lunacy@g'
+	's@/opt/icons8/lunacy/Assets/LunacyLogo.png@lunacy@g'\
+	/usr/share/applications/lunacy.desktop
 rm -rf Lunacy.deb
