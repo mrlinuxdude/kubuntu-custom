4da66fbd2605168e510dd12581920c808f677eed
Raul Dipeas <rauldipeas@disroot.org>
2022-06-13 05:48:51 -0300

    ^scripts ^postinst

--- postinst/update/lsd.sh
+++ postinst/update/lsd.sh
@@ -5,6 +5,6 @@ rm -rf lsd*.deb
 aria2c --console-log-level=error --summary-interval=0\
 	"$(wget -qO-\
 	https://api.github.com/repos/Peltoche/lsd/releases|\
-	grep browser_download_url|grep download|grep amd64.deb|head -n1|cut -d '"' -f4)"
+	grep browser_download_url|grep download|grep amd64.deb|grep -v musl|head -n1|cut -d '"' -f4)"
 sudo apt install ./lsd*.deb
 rm -rf lsd*.deb
