28d88cb0c05f505b74839d5ba3ed71700f10f3d4
Raul Dipeas <rauldipeas@disroot.org>
2022-06-13 05:16:55 -0300

    ^postinst

---
+++ postinst/optional/tonelib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+# ToneLib
+rm -rf ToneLib*.deb
+aria2c --console-log-level=error --summary-interval=0\
+	https://tonelib.net/download/ToneLib-BassDrive-amd64.deb
+aria2c --console-log-level=error --summary-interval=0\
+	https://tonelib.net/download/ToneLib-TubeWarmth-amd64.deb
+aria2c --console-log-level=error --summary-interval=0\
+	https://tonelib.net/download/ToneLib-Zoom-amd64.deb
+aria2c --console-log-level=error --summary-interval=0\
+	https://plugins4free.com/get_plug/ToneLib-GFX-amd64.deb
+sudo apt install ./ToneLibe*.deb
+rm -rf ToneLib*.deb
