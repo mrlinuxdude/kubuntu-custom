ca141737eea01a47102fcc2589620194b96855ba
Raul Dipeas <rauldipeas@disroot.org>
2022-06-17 01:54:21 -0300

    ^postinst

--- testing.sh
+++ testing.sh
@@ -12,3 +12,4 @@ bash -x kubuntu-custom/postinst/testing/nerd-fonts-hack.sh #external-package #ma
 bash -x kubuntu-custom/postinst/testing/plasma-hud.sh #external-package #makepkg #binary
 bash -x kubuntu-custom/postinst/testing/obs-multi-rtmp.sh #external-package #makepkg #aur
 bash -x kubuntu-custom/postinst/testing/syncthingtray.sh #external-package #makepkg #binary
+bash -x kubuntu-custom/postinst/testing/transfuse.sh #no-packaged
