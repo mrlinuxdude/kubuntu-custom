da54cb0d00465c0b9fb536c47d5463f0b740fd7d
Raul Dipeas <rauldipeas@disroot.org>
2022-07-09 13:38:43 -0300

    ^scripts ^postinst

--- postinst/update/bitwarden.sh
+++ postinst/update/bitwarden.sh
@@ -2,7 +2,11 @@
 set -e
 # Bitwarden
 rm -rf Bitwarden*.deb
+#aria2c --console-log-level=error --summary-interval=0\
+#    'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
 aria2c --console-log-level=error --summary-interval=0\
-    'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
+	"$(wget -qO-\
+	https://api.github.com/repos/bitwarden/clients/releases|\
+	grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 sudo apt install ./Bitwarden*.deb
 rm -rf Bitwarden*.deb
--- scripts/apps/bitwarden.sh
+++ scripts/apps/bitwarden.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+#aria2c --console-log-level=error --summary-interval=0\
+#    'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
 aria2c --console-log-level=error --summary-interval=0\
-    'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
+	"$(wget -qO-\
+	https://api.github.com/repos/bitwarden/clients/releases|\
+	grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 apt install -y ./Bitwarden*.deb
