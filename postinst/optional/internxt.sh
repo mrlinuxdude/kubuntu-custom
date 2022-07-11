edf693b95dab61e634161c19f5a9838cf12d8a26
Raul Dipeas <rauldipeas@disroot.org>
2022-07-11 14:33:14 -0300

    ^postinst

---
+++ postinst/optional/internxt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Internxt
+rm -rf drive.deb
+aria2c --console-log-level=error --summary-interval=0\
+    https://internxt.com/downloads/drive.deb
+sudo apt install ./drive.deb
+rm -rf drive.deb
