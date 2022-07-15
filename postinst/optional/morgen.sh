59555eb33dd5c46b699891aa2714370e34ee82e7
Raul Dipeas <rauldipeas@disroot.org>
2022-07-15 10:24:56 -0300

    ^postinst

---
+++ postinst/optional/morgen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Morgen
+rm -rfv morgen*.deb
+aria2c --console-log-level=error --summary-interval=0\
+    "$(wget -qO- https://www.morgen.so/onboarding/download|grep deb|cut -d '"' -f82)"
+sudo apt install ./morgen*.deb
+rm -rfv morgen*.deb
