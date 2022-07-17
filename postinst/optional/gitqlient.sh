8d24dd6091d9675469684de75de44dec060eecdc
Raul Dipeas <rauldipeas@disroot.org>
2022-07-16 22:42:31 -0300

    ^postinst

--- postinst/optional/gitqlient.sh
+++ postinst/optional/gitqlient.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
-# Heroic
+# GitQlient
 rm -rf gitqlient*.deb
 aria2c --console-log-level=error --summary-interval=0\
-    "$(wget -qO- https://api.github.com/repos/francescmm/GitQlient/releases|\
+    "$(wget -qO-\
+    https://api.github.com/repos/francescmm/GitQlient/releases|\
     grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 sudo apt install ./gitqlient*.deb
 rm -rf gitqlient*.deb
