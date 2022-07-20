169224b6b03ab2d5f6d40f302a3b3aff50973b4e
Raul Dipeas <rauldipeas@disroot.org>
2022-07-20 16:34:16 -0300

    ^postinst

--- postinst/core/swap.sh
+++ postinst/core/swap.sh
@@ -2,6 +2,6 @@
 set -e
 # Swap
 sudo swapoff /swapfile
-sudo dd if=/dev/zero of=/swapfile bs=1M count=4096
+sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
 sudo mkswap /swapfile
 sudo swapon /swapfile
