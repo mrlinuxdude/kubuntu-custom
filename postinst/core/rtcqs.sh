b863059a15eaeba2fd88e417e60623ba7dbd589b
Raul Dipeas <rauldipeas@disroot.org>
2022-07-27 11:20:53 -0300

    ^postinst

--- postinst/core/rtcqs.sh
+++ postinst/core/rtcqs.sh
@@ -7,7 +7,7 @@ pip install -q rtcqs
 mkdir -p "$HOME"/.local/share/{applications,icons}
 wget -qO\
 	"$HOME"/.local/share/applications/rtcqs.desktop\
-	https://codeberg.org/rtcqs/rtcqs/raw/branch/main/rtcqs.desktop
+	https://github.com/autostatic/rtcqs/raw/branch/main/rtcqs.desktop
 wget -qO\
 	"$HOME"/.local/share/icons/rtcqs.svg\
-	https://codeberg.org/rtcqs/rtcqs/raw/branch/main/rtcqs_logo.svg
+	https://github.com/autostatic/rtcqs/raw/branch/main/rtcqs_logo.svg
