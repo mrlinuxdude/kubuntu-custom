1607dac60e8bce7daecfe5c5f3de3de06ba71e45
Raul Dipeas <rauldipeas@disroot.org>
2022-08-03 03:44:48 -0300

    ^postinst

--- postinst/optional/blender.sh
+++ postinst/optional/blender.sh
@@ -3,13 +3,28 @@ set -e
 # Blender
 #sudo add-apt-repository -y ppa:savoury1/blender
 #sudo nala install blender-git #blender #blender-lts
+sudo nala install audacity
 BLENDER_VER=$(curl -sL https://ftp.nluug.nl/pub/graphics/blender/release|grep Blender3.|tail -n1|cut -d \" -f6)
 BLENDER_FILE=$(curl -sL https://ftp.nluug.nl/pub/graphics/blender/release/$BLENDER_VER|grep .tar.xz|tail -n1|cut -d \" -f6)
-aria2c --console-log-level=error --summary-interval=0 https://ftp.nluug.nl/pub/graphics/blender/release/$BLENDER_VER$BLENDER_FILE
-tar -xf blender*.tar.xz
-rm -rf $HOME/.local/share/blender/
-mv blender*/ $HOME/.local/share/blender/
-rm -rf blender*.tar.xz
-mkdir -p $HOME/.local/share/applications
-cp -rf $HOME/.local/share/blender/blender.desktop $HOME/.local/share/applications/
-sed -i "s@Exec=blender@Exec=$HOME/.local\/share\/blender\/blender@g" $HOME/.local/share/applications/blender.desktop
+aria2c --console-log-level=error --summary-interval=0\
+    https://ftp.nluug.nl/pub/graphics/blender/release/$BLENDER_VER$BLENDER_FILE
+tar -xf\
+    blender*.tar.xz
+rm -rf\
+    $HOME/.local/share/blender/
+mv\
+    blender*/\
+    $HOME/.local/share/blender/
+rm -rf\
+    blender*.tar.xz
+mkdir -p\
+    $HOME/.local/share/applications
+cp -rf\
+    $HOME/.local/share/blender/blender.desktop\
+    $HOME/.local/share/applications/
+sed -i\
+    "s@Exec=blender@Exec=sh -c \"audacity\&sleep 1\;$HOME/.local/share/blender/blender\;killall -9 audacity\"@g"\
+    $HOME/.local/share/applications/blender.desktop
+aria2c --console-log-level=error --summary-interval=0\
+    https://github.com/tin2tin/audacity_tools_for_blender/archive/main.zip
+unzip audacity_tools_for_blender-main.zip -d "$HOME"/.config/blender/"$(ls ~/.config/blender/)"/scripts/addons/
